7468b2a53555bdc12203ee5d8707e97bf251e7fb
Vladimir Horev <VShorev@yandex.ru>
2024-02-27 08:02:50 +0300

    HashRouter++++

--- src/Mainpage.js
+++ src/Mainpage.js
@@ -20,7 +20,7 @@ function App() {
 
     useEffect(() => {
         try {
-            fetch("http://api.valantis.store:40000/",
+            fetch("https://api.valantis.store:41000/",
                 {
                     method: 'POST',
                     headers: {
@@ -35,7 +35,7 @@ function App() {
             )
                 .then(resp => resp.json())
                 .then((response) => {
-                    fetch("http://api.valantis.store:40000/",
+                    fetch("https://api.valantis.store:41000/",
                         {
                             method: 'POST',
                             headers: {
@@ -84,7 +84,7 @@ function App() {
 
     // Возвращает с апи все по одной цене
     const SortByPrice = () => {
-        fetch("http://api.valantis.store:40000/",
+        fetch("https://api.valantis.store:41000/",
             {
                 method: 'POST',
                 headers: {
@@ -99,7 +99,7 @@ function App() {
         )
             .then(resp => resp.json())
             .then((response) => {
-                fetch("http://api.valantis.store:40000/",
+                fetch("https://api.valantis.store:41000/",
                     {
                         method: 'POST',
                         headers: {
